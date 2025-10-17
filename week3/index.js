996e3049d1890312a8bed384642d6e5703fda713
daV1dLiu132 <davidliuzongwei@gmail.com>
2025-10-17 11:51:32 +0000

    added week3

--- week3/index.js
+++ week3/index.js
@@ -1,1 +1,28 @@
+function EmployeeInfo(name, Salary){
+    console.log("Wellcome " + name + " Your monthly Salary is "+ Salary)
+}
 
+console.log ("This is my first progame")
+
+var EmpName = "John"
+var EmpSalary = 50000
+EmployeeInfo(EmpName, EmpSalary)
+
+const EmpSkills= (skills) =>{
+    console.log("Expert in " + skills)
+}
+EmpSkills("java")
+
+const student = require('./StudentInfo')
+const person = require('./Person')
+
+console.log("Student Name:" + student.getName())
+console.log(student.Location())
+console.log(student.dob)
+
+console.log(student.Studentgrade())
+console.log("grade is"+ student.Studentgrade(55))
+
+person1 = new person("Jim", "USA", "myemail@email.com")
+console.log("using Person Module", person1.getPersonInfo())
+console.log("Programe ended")
