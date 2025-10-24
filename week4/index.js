d67f5b8948934149ffa9582f6776d1789c7610dc
daV1dLiu132 <davidliuzongwei@gmail.com>
2025-10-24 10:28:23 +0000

    add index.js

--- week4/index.js
+++ week4/index.js
@@ -1,1 +1,9 @@
-
+var express = require("express")
+var fs = require("fs")
+var app = express()
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended : true}));
+app.get('/', function(req,res){
+    res.send("hello it is my first express application")
+})
+app.listen(5000, function(){console.log("server is running on port 5000")})
