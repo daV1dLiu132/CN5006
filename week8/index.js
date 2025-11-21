17872fe32a9ecf22c00aeebfbed8ff616f2c85f5
daV1dLiu132 <davidliuzongwei@gmail.com>
2025-11-21 12:55:08 +0000

    2

--- week8/index.js
+++ week8/index.js
@@ -19,26 +19,16 @@ age: Number, Gender:String, Salary:Number
 // creating model named as modelname with collection named as personCollection
 const person_doc = mongoose.model('modelname', PersonScheme,'personCollection');
 // creating a single document
-const doc1 = new person_doc({ name: 'Yousuf',age:44,Gender:"Male",Salary:3456 }
-);
 // adding one document in the collection
-doc1
- .save()
- .then((doc1) => {
- console.log("New Article Has been Added Into Your DataBase.",doc1);
- })
- .catch((err) => {
- console.error(err);
- });
 
-manypersons=[{ name: 'Simon', age:42, Gender:"Male",Salary:3456}
-    ,{ name: 'Neesha', age:23, Gender:"Female",Salary:1000}
-    ,{ name: 'Mary', age:27, Gender:"Female",Salary:5402}
-    ,{ name: 'Mike', age:40, Gender:"Male",Salary:4519}
-]
-person_doc.insertMany(manypersons).then(function(){
-    console.log("Data inserted")
-})
-.catch(function(error){
-    console.log(error)
-});
+
+
+
+person_doc.updateMany({Gender: "Female"},{Salary:5555})
+   .exec()
+    .then(docs=>{
+        console.log("update");
+        console.log(docs);
+    }).catch(function(error){
+        console.error(err);
+    });
