3c3fa52139906638ae89a36caba814b1e5963bb8
daV1dLiu132 <davidliuzongwei@gmail.com>
2025-12-05 11:15:48 +0000

    1

--- OnlineLibrary/backend/schema.js
+++ OnlineLibrary/backend/schema.js
@@ -1,6 +1,12 @@
-const BookSchema = newmongoose.Schema({
-    booktitle:{
-        type:String,
-        
-    }
-})
+let mongoose= require('mongoose')
+const BookScheme= new mongoose.Schema({
+ booktitle:{
+ type:String,
+ required:true
+ },
+ PubYear:Number,
+ author:String,
+ Topic:String,
+ formate:String
+ })
+ module.exports= mongoose.model('bookmodel',BookScheme,'BookCollection2') 
